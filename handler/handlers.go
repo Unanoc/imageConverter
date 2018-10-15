0e3cc4ca6491f379bcaf779a89514628d13bf79c
Unanoc <leedaniil98@gmail.com>
2018-10-15 20:50:11 +0300

    bugs fixed

--- handler/handlers.go
+++ handler/handlers.go
@@ -29,21 +29,29 @@ func ConvertHandler(ctx *fasthttp.RequestCtx) {
 	formData, err := ctx.FormFile("fileToUpload")
 	if err != nil {
 		logger.LoggerInfo(err.Error())
+		ctx.SetBody([]byte(err.Error()))
+		return
 	}
 
 	inputFile, err := formData.Open()
 	if err != nil {
 		logger.LoggerInfo(err.Error())
+		ctx.SetBody([]byte(err.Error()))
+		return
 	}
 
 	inputPNG, err := png.Decode(inputFile)
 	if err != nil {
 		logger.LoggerInfo(err.Error())
+		ctx.SetBody([]byte(err.Error()))
+		return
 	}
 
 	pngBytes, err := ioutil.ReadAll(inputFile)
 	if err != nil {
 		logger.LoggerInfo(err.Error())
+		ctx.SetBody([]byte(err.Error()))
+		return
 	}
 
 	if _, err := os.Stat(imagesDirPath); os.IsNotExist(err) {
@@ -52,21 +60,25 @@ func ConvertHandler(ctx *fasthttp.RequestCtx) {
 	_ = os.Mkdir(imagesDirPath, 0777)
 
 	imagePath := imagesDirPath + fmt.Sprintf("%x", md5.Sum(pngBytes)) + ".jpg"
-	jpgImgFile, err := os.Create(string(imagePath))
+	jpgImgFile, err := os.Create(imagePath)
 	if err != nil {
 		logger.LoggerInfo(err.Error())
+		ctx.SetBody([]byte(err.Error()))
+		return
 	}
 	defer jpgImgFile.Close()
 
 	err = jpeg.Encode(jpgImgFile, inputPNG, nil)
 	if err != nil {
 		logger.LoggerInfo(err.Error())
+		ctx.SetBody([]byte(err.Error()))
+		return
 	}
 
 	// remove image in 3 minutes
 	go removeImageInTime(imagePath)
 	fullImageUrl := string(ctx.Host()) + "/" + imagePath
-	ctx.Write([]byte(fullImageUrl))
+	ctx.SetBody([]byte(fullImageUrl))
 }
 
 func removeImageInTime(pathToFile string) {
